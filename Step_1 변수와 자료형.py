1a1af3b838096e9a639745e4614788e65e2d748b
ohsang <ast6108@naver.com>
2023-09-14 19:14:55 +0900

    study

---
+++ Step_1 변수와 자료형.py
@@ -0,0 +1,40 @@
+print("정수 3 출력"
+      "실수 -3.2 출력 :")
+print(3)
+print(3.2)
+
+
+print(" type()은 괄호 안에 입력한 자료의 유형을 알려주는 함수"
+      "정수 3과 실수 -3.2 대한 자료 유형 출력:")
+print(type(3))
+print(type(-3.2))
+
+print("a, b, c 변수 선언: ")
+a = 4
+b = 3.2
+c = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print("a, c 변수를 통한 나눗셈 연산: ")
+
+print(a/c)
+print(a % c)
+print(a//c)
+
+print("제곱: ")
+print(a**c)
+
+print("정수형 변환")
+print(int(6.1))
+print(int(-6.2))
+
+print("실수형 변환")
+print(float(5))
+print(abs(-6.2))
+
+print("제곱 출력")
+print(pow(4.2))
